22967a9c210e6461be6a4263fa07314b1018321d
trevor oakley <sales@merrows.co.uk>
2020-08-13 06:37:39 +0000

    init

---
+++ index.js
@@ -0,0 +1,176 @@
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const express = require('express');
+
+const app = express();
+//const { Pool, Client } = require('pg')
+const pg = require('pg');
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+request = require('request');
+const bodyParser = require('body-parser');
+var wget = require('node-wget');
+var url = require('url');
+var path = require('path');
+
+mongoose.connect('mongodb://127.0.0.1:27017/soildb');
+
+const privateKey = fs.readFileSync('privkey.pem', 'utf8');
+const certificate = fs.readFileSync('fullchain.pem', 'utf8');
+const ca = fs.readFileSync('0000_csr-certbot.pem', 'utf8');
+
+
+const credentials= {
+  key: privateKey,
+  cert: certificate
+}
+
+// ref - Global Soil Dataset for earth system modelling
+// https://agupubs.onlinelibrary.wiley.com/doi/pdf/10.1002/2013MS000293
+// Examples are National Cooperative Soil Survey of the United States
+var soilSchema = new mongoose.Schema({
+  VolWater10kPa : { type: Number, default: null },
+  VolWater33kPa : { type: Number, default: null },
+  VolWater150kPa : { type: Number, default: null },
+  Longitude : {type: Number, default: null},
+  Latitude : {type: Number, default: null},
+  InstrIdentity : {type: Number, default: null},
+  ReadingDate : {type: Date, default: null},
+});
+
+var soilDB = mongoose.model("soildata", soilSchema);
+
+app.get("/", function(req, res) {
+    res.json({ message: "Welcome to IOSET - API services" });
+});
+
+
+app.get("/api/ping", function(req, res) {
+    res.json({ message: "pong" });
+});
+
+app.use('/', express.static(path.join(__dirname, '/html')));
+
+//app.use((req, res) => {
+//	res.send('Hello there !');
+//});
+//app.use(function(req, res, next) {
+//    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, x-access-token');
+//    next();
+//});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// add token acceptance here.
+// get token from blockchain and check against sending token from rest api call
+//
+//
+//
+//
+//
+
+
+app.post("/api/getinstr", function(req, res) {
+    var InstrIdentity = req.body.InstrIdentity;
+
+
+  soilDB.find({ InstrIdentity : InstrIdentity}, function(err, doc){
+    if(doc == null){
+         res.json({ message: "no data", "doc": null });
+    }else{
+         res.json({ message: "correct", "doc": doc});
+    }
+  });
+
+
+});
+
+
+
+
+app.post("/api/getallinstr", function(req, res) {
+    var minlongitude = req.body.minlongitude;
+    var maxlongitude = req.body.maxlongitude;
+    var minlatitude = req.body.minlatitude;
+    var maxlatitude = req.body.maxlatitude;
+console.log(minlongitude);
+console.log(maxlongitude);
+console.log(minlatitude);
+console.log(maxlatitude);
+groupDB.
+  find({
+    Longitude: { $gt: minlongitude, $lt: maxlongitude },
+    Latitude: { $gt: minlatitude, $lt: maxlatitude }
+  }, function (err, doc) {
+	  if (err) {
+		  res.json({"message": "error", "doc": null}) 
+	  } else {
+		  res.json({ "message": "Correct", "doc": doc})
+	  }
+  });
+
+
+});
+
+app.post("/api/addinstr", function(req, res) {
+    var VolWater10kPa = req.body.VolWater10kPa;
+    var VolWater33kPa = req.body.VolWater33kPa;
+    var VolWater100kPa = req.body.VolWater100kPa;
+    var Longitude = req.body.Longitude;
+    var Latitude = req.body.Latitude;
+    var InstrIdentity = req.body.InstrIdentity;
+    var ReadingDate = req.body.ReadingDate;
+console.log("instr id = "+ InstrIdentity);
+console.log("req body = "+ JSON.stringify(req.body));
+    // var testBuffer = req.body.image;
+    var soilCreate = new soilDB({
+    VolWater10kPa : VolWater10kPa,
+    VolWater33kPa : VolWater33kPa,
+    VolWater100kPa : VolWater100kPa,
+    Longitude : Longitude,
+    Latitude : Latitude,
+    InstrIdentity: InstrIdentity,
+    ReadingDate: ReadingDate
+    });
+
+  soilDB.findOne({ InstrIdentity : InstrIdentity, ReadingDate: ReadingDate}, function(err, doc){
+    if(doc == null){
+      soilCreate.save(function(err, doc){
+        if(err) throw err;
+
+         res.json({ message: "Correct" });
+      });
+    }else{
+         res.json({ message: "soil measurement exists"});
+    }
+  });
+
+
+});
+
+
+// Starting both http & https servers
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3000, () => {
+    console.log('HTTP Server running on port 3000');
+});
+//
+//const httpServer = http.createServer(app);
+
+//httpServer.listen(3000, () => {
+  //  console.log('HTTP Server running on port 3000');
+//});
